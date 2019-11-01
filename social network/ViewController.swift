0b93eaf13d033f5389514c2383b7e4e1b3a62b3a
Roger Goh <rogergoh@outlook.my>
2019-11-02 07:37:49 +0800

    Initial Commit

---
+++ social network/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  social network
+//
+//  Created by Julia van Meurs on 2/11/19.
+//  Copyright © 2019 Roger Goh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
